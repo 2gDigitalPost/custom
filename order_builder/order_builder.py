7061ed91c246273973632f65b60882201719821d
TStand90 <tstand90@gmail.com>
2016-01-08 13:26:19 -0800

    PEP8 spacing and removing useless comments.

--- order_builder/order_builder.py
+++ order_builder/order_builder.py
@@ -7299,61 +7299,63 @@ class TaskEditWdg(BaseRefreshWdg):
         my.parent_sk = ''
         my.parent_pyclass = ''
         my.order_sk = ''
-        my.groups = Environment.get_group_names() 
+        my.groups = Environment.get_group_names()
 
     def get_assigned_group_select(my, assigned):
         where_str = "('user','client','compression supervisor','edit supervisor','machine room supervisor','media vault supervisor','qc supervisor','sales supervisor','scheduling supervisor','streamz','executives','admin','management','office employees','it')"
         group_search = Search("sthpw/login_group")
         group_search.add_where("\"login_group\" not in %s" % where_str)
         groups = group_search.get_sobjects()
-        
+
         group_sel = SelectWdg('task_assigned_group_select')
         if len(groups) > 0:
-            group_sel.append_option('--Select--','')
+            group_sel.append_option('--Select--', '')
             if assigned:
                 group_sel.set_value(assigned)
             else:
                 group_sel.set_value('')
             for group in groups:
-                group_sel.append_option(group.get_value('login_group'),group.get_value('login_group'))
+                group_sel.append_option(group.get_value('login_group'), group.get_value('login_group'))
         return group_sel
 
     def get_assigned_select(my, assigned):
         workers_search = Search("sthpw/login")
-        workers_search.add_filter('location','internal')
-        workers_search.add_filter('license_type','user')
+        workers_search.add_filter('location', 'internal')
+        workers_search.add_filter('license_type', 'user')
         workers = workers_search.get_sobjects()
         work_sel = SelectWdg('task_assigned_select')
         if len(workers) > 0:
-            work_sel.append_option('--Select--','')
+            work_sel.append_option('--Select--', '')
             if assigned:
                 work_sel.set_value(assigned)
             else:
                 work_sel.set_value('')
             for worker in workers:
-                work_sel.append_option(worker.get_value('login'),worker.get_value('login'))
+                work_sel.append_option(worker.get_value('login'), worker.get_value('login'))
         return work_sel
-        
+
     def get_statuses_select_from_task_pipe(my, status, pipe_code, search_type, task_sk, user_is_scheduler):
         # gets the processes from the sobject's pipeline
-        statuses = ['Pending','Ready','On Hold','Client Response','Fix Needed','Rejected','In Progress','DR In Progress','Amberfin01 In Progress','Amberfin02 In Progress','BATON In Progress','Export In Progress','Need Buddy Check','Buddy Check In Progress','Completed']
+        statuses = ['Pending', 'Ready', 'On Hold', 'Client Response', 'Fix Needed', 'Rejected', 'In Progress',
+                    'DR In Progress', 'Amberfin01 In Progress', 'Amberfin02 In Progress', 'BATON In Progress',
+                    'Export In Progress', 'Need Buddy Check', 'Buddy Check In Progress', 'Completed']
         task_select = SelectWdg('task_stat_select')
-        task_select.add_attr('old_status',status)
+        task_select.add_attr('old_status', status)
         task_select.add_attr('id','status_%s' % task_sk)
         if len(statuses) > 0:
-            task_select.append_option('--Select--','')
+            task_select.append_option('--Select--', '')
             task_select.set_value(status)
             for stat in statuses:
                 if stat == 'On Hold' and user_is_scheduler:
-                    task_select.append_option(stat,stat)
+                    task_select.append_option(stat, stat)
                 if stat == 'Approved':
-                    task_select.append_option('Rejected','Rejected')
-                task_select.append_option(stat,stat)
+                    task_select.append_option('Rejected', 'Rejected')
+                task_select.append_option(stat, stat)
                 if stat == 'Approved':
-                    task_select.append_option('Completed','Completed')
-       
+                    task_select.append_option('Completed', 'Completed')
+
         return task_select
-    
+
     def fix_date(my, date):
         #This is needed due to the way Tactic deals with dates (using timezone info), post v4.0
         from pyasm.common import SPTDate
@@ -7363,13 +7365,18 @@ class TaskEditWdg(BaseRefreshWdg):
             return_date = date_obj.strftime("%Y-%m-%d  %H:%M")
         return return_date
 
-    def get_display(my):   
-#        task_edit_time = time.time()
+    def get_display(my):
         my.task_code = my.kwargs.get('task_code')
         my.parent_sk = my.kwargs.get('parent_sk')
         my.order_sk = my.kwargs.get('order_sk')
         obs = OBScripts(order_sk=my.order_sk)
-        pyclass_dict = {'twog/title': 'TitleRow','twog/order': 'OrderTable', 'twog/proj': 'ProjRow', 'twog/work_order': 'WorkOrderRow', 'twog/equipment_used': 'EquipmentUsedRow'}
+        pyclass_dict = {
+            'twog/title': 'TitleRow',
+            'twog/order': 'OrderTable',
+            'twog/proj': 'ProjRow',
+            'twog/work_order': 'WorkOrderRow',
+            'twog/equipment_used': 'EquipmentUsedRow'
+        }
 
         groups_str = ''
         user_group_names = Environment.get_group_names()
@@ -7383,99 +7390,104 @@ class TaskEditWdg(BaseRefreshWdg):
             user_is_scheduler = True
 
         table = Table()
-        table.add_attr('class','task_table')
-        table.add_attr('width','100%s' % '%')
-        if my.task_code in ['',None]:
+        table.add_attr('class', 'task_table')
+        table.add_attr('width', '100%')
+        if my.task_code in ['', None]:
             table.add_row()
             table.add_cell('')
         else:
             my.parent_pyclass = pyclass_dict[my.parent_sk.split('?')[0]]
             table.add_style('background-color: 90a0b5;')
             task_search = Search("sthpw/task")
-            task_search.add_filter('code',my.task_code)
+            task_search.add_filter('code', my.task_code)
             tasks_found = task_search.get_sobjects()
             if len(tasks_found) > 0:
                 task = tasks_found[0]
                 hours_search = Search('sthpw/work_hour')
-                hours_search.add_filter('task_code',task.get_code())
+                hours_search.add_filter('task_code', task.get_code())
                 hours = hours_search.get_sobjects()
                 hours_added = 0
                 for hour in hours:
                     hour_num = hour.get_value('straight_time')
-                    if hour_num in [None,'']:
+                    if hour_num in [None, '']:
                         hour_num = 0
                     hour_num = float(hour_num)
                     hours_added = float(hours_added) + hour_num
                 pipe_code = task.get_value('pipeline_code')
                 if 'task' not in pipe_code:
                     pipe_code = 'task'
-                task_select = my.get_statuses_select_from_task_pipe(task.get_value('status'),pipe_code,'sthpw/pipeline',task.get_search_key(), user_is_scheduler)
+                task_select = my.get_statuses_select_from_task_pipe(task.get_value('status'),
+                                                                    pipe_code,
+                                                                    'sthpw/pipeline',
+                                                                    task.get_search_key(),
+                                                                    user_is_scheduler)
                 worker_select = my.get_assigned_select(task.get_value('assigned'))
                 group_select = my.get_assigned_group_select(task.get_value('assigned_login_group'))
                 table.add_row()
                 top = table.add_cell('<b>Assignment Information<b>')
-                top.add_attr('colspan','4')
-                top.add_attr('align','center')
+                top.add_attr('colspan', '4')
+                top.add_attr('align', 'center')
                 top.add_style('background-color: #6789b7;')
                 table.add_row()
-                table.add_cell('Status: ') 
-                table.add_cell(task_select) 
-                table.add_cell('Assigned: ') 
-                table.add_cell(worker_select) 
+                table.add_cell('Status: ')
+                table.add_cell(task_select)
+                table.add_cell('Assigned: ')
+                table.add_cell(worker_select)
                 table.add_row()
                 table.add_cell('Hours Added:')
                 table.add_cell(hours_added)
                 table.add_cell('Add Hours:')
-                table.add_cell('<input type="text" class="hour_adder" value=""/>') 
+                table.add_cell('<input type="text" class="hour_adder" value=""/>')
                 table.add_row()
-                table.add_cell('Priority: ') 
-                table.add_cell('<input type="text" class="priority_setter" value="%s"/>' % task.get_value('priority')) 
-                table.add_cell('Assigned Group: ') 
-                table.add_cell(group_select) 
-                bid_start = CalendarInputWdg(name='task_bid_start') #4.2
-                bid_start.set_option('show_time','true')
-                bid_start.set_option('show_activator','true')
-                bid_start.set_option('display_format','MM/DD/YYYY HH:MM')
-                bid_start.set_option('time_input_default','5:00 PM')
-                if task.get_value('bid_start_date') not in ['',None]:
+                table.add_cell('Priority: ')
+                table.add_cell('<input type="text" class="priority_setter" value="%s"/>' % task.get_value('priority'))
+                table.add_cell('Assigned Group: ')
+                table.add_cell(group_select)
+                bid_start = CalendarInputWdg(name='task_bid_start') # 4.2
+                bid_start.set_option('show_time', 'true')
+                bid_start.set_option('show_activator', 'true')
+                bid_start.set_option('display_format', 'MM/DD/YYYY HH:MM')
+                bid_start.set_option('time_input_default', '5:00 PM')
+                if task.get_value('bid_start_date') not in ['', None]:
                     bid_start.set_option('default', my.fix_date(task.get_value('bid_start_date')))
-                bid_end = CalendarInputWdg(name='task_bid_end') #4.2
-                bid_end.set_option('show_time','true')
-                bid_end.set_option('show_activator','true')
-                bid_end.set_option('display_format','MM/DD/YYYY HH:MM')
-                bid_end.set_option('time_input_default','5:00 PM')
-                if task.get_value('bid_end_date') not in ['',None]:
+                bid_end = CalendarInputWdg(name='task_bid_end') # 4.2
+                bid_end.set_option('show_time', 'true')
+                bid_end.set_option('show_activator', 'true')
+                bid_end.set_option('display_format', 'MM/DD/YYYY HH:MM')
+                bid_end.set_option('time_input_default', '5:00 PM')
+                if task.get_value('bid_end_date') not in ['', None]:
                     bid_end.set_option('default', my.fix_date(task.get_value('bid_end_date')))
                 table.add_row()
                 bs = table.add_cell('Bid Start: ')
-                bs.add_attr('nowrap','nowrap')
+                bs.add_attr('nowrap', 'nowrap')
                 table.add_cell(bid_start)
                 be = table.add_cell('Due Date: ')
-                be.add_attr('nowrap','nowrap')
+                be.add_attr('nowrap', 'nowrap')
                 table.add_cell(bid_end)
                 table.add_row()
                 acs = table.add_cell('Actual Start: ')
-                acs.add_attr('nowrap','nowrap')
+                acs.add_attr('nowrap', 'nowrap')
                 acsv = my.fix_date(task.get_value('actual_start_date'))
-                if acsv in ['',None]:
+                if acsv in ['', None]:
                     acsv = 'Has not begun'
                 table.add_cell(acsv)
                 ace = table.add_cell('Actual End: ')
                 acev = my.fix_date(task.get_value('actual_end_date'))
-                if acev in ['',None]:
+                if acev in ['', None]:
                     acev = 'Has not ended'
-                ace.add_attr('nowrap','nowrap')
+                ace.add_attr('nowrap', 'nowrap')
                 table.add_cell(acev)
                 table.add_row()
                 doubl = table.add_cell('<input type="button" value="Save"/>')
-                doubl.add_attr('colspan','4')
-                doubl.add_attr('align','center')
+                doubl.add_attr('colspan', '4')
+                doubl.add_attr('align', 'center')
                 doubl.add_behavior(obs.get_save_task_info_behavior(task.get_search_key(), my.parent_sk, my.parent_pyclass))
             else:
                 table.add_row()
                 table.add_cell('')
 
         return table
+
 
 class OperatorErrorDescriptPopupWdg(BaseRefreshWdg): 
     def init(my):
