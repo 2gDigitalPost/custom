b158d06af0e5ba126198670eeda8499ac486b493
TStand90 <tstand90@gmail.com>
2016-02-25 10:25:03 -0800

    platform_modified.py to update last_modified in platform table.

---
+++ scripts/trigger/platform_modified.py
@@ -0,0 +1,33 @@
+import traceback
+import datetime
+
+
+def main(server=None, input=None):
+    """
+    :param server: the TacticServerStub object
+    :param input: a dict with data like like search_key, search_type, sobject, and update_data
+    :return: None
+    """
+    if not input:
+        input = {}
+
+    try:
+        sk = input.get('search_key')
+        timestamp = datetime.datetime.now()
+        server.update(sk, {'last_modified': timestamp}, triggers=False)
+    except AttributeError as e:
+        traceback.print_exc()
+        print str(e) + '\nMost likely the server object does not exist.'
+        raise e
+    except KeyError as e:
+        traceback.print_exc()
+        print str(e) + '\nMost likely the input dictionary does not exist.'
+        raise e
+    except Exception as e:
+        traceback.print_exc()
+        print str(e)
+        raise e
+
+
+if __name__ == '__main__':
+    main()
