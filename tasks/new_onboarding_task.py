d1619bacd3964c961d11eb8e2b73c62d7d0b7a39
TStand90 <tstand90@gmail.com>
2016-04-26 14:25:28 -0700

    Moved static functions to task_utils.py

--- tasks/new_onboarding_task.py
+++ tasks/new_onboarding_task.py
@@ -1,11 +1,9 @@
 from tactic.ui.common import BaseRefreshWdg
-from tactic.ui.input import TextInputWdg, TextAreaInputWdg
-from tactic.ui.widget import CalendarInputWdg
 
 from pyasm.web import DivWdg
 from pyasm.widget import SubmitWdg
 
-from order_builder.order_builder_utils import get_label_widget, get_select_widget_from_search_type
+import task_utils
 
 
 class NewOnboardingTask(BaseRefreshWdg):
@@ -16,57 +14,14 @@ class NewOnboardingTask(BaseRefreshWdg):
         page_label = "New Onboarding Task"
         outer_div.add(page_label)
 
-        self.get_process_widget(outer_div)
-        self.get_description_widget(outer_div)
-        self.get_priority_widget(outer_div)
-        self.get_assigned_to_select_widget(outer_div)
-        self.get_due_date_widget(outer_div)
+        task_utils.get_process_widget(outer_div)
+        task_utils.get_description_widget(outer_div)
+        task_utils.get_priority_widget(outer_div)
+        task_utils.get_assigned_to_select_widget(outer_div)
+        task_utils.get_due_date_widget(outer_div)
         self.get_submit_widget(outer_div)
 
         return outer_div
-
-    @staticmethod
-    def get_process_widget(outer_div):
-        process_input = TextInputWdg()
-        process_input.set_name('process')
-
-        outer_div.add(get_label_widget('Process'))
-        outer_div.add(process_input)
-
-    @staticmethod
-    def get_assigned_to_select_widget(outer_div):
-        user_search_filters = [('login_group', 'onboarding')]
-        assigned_to_select_widget = get_select_widget_from_search_type('sthpw/login_in_group', 'login', 'login', 'code',
-                                                                       user_search_filters)
-        assigned_to_select_widget.set_name('assigned')
-
-        outer_div.add(get_label_widget('Assigned To'))
-        outer_div.add(assigned_to_select_widget)
-
-    @staticmethod
-    def get_description_widget(outer_div):
-        description_input = TextAreaInputWdg(name='description')
-        description_input.add_class('description')
-
-        outer_div.add(get_label_widget('Description'))
-        outer_div.add(description_input)
-
-    @staticmethod
-    def get_priority_widget(outer_div):
-        priority_input = TextInputWdg()
-        priority_input.set_name('priority')
-
-        outer_div.add(get_label_widget('Priority'))
-        outer_div.add(priority_input)
-
-    @staticmethod
-    def get_due_date_widget(outer_div):
-        due_date_wdg = CalendarInputWdg('Due Date')
-        due_date_wdg.add_class('due_date')
-        due_date_wdg.set_name('due_date')
-
-        outer_div.add(get_label_widget('Due Date'))
-        outer_div.add(due_date_wdg)
 
     def get_submit_widget(self, outer_div):
         submit_button = SubmitWdg('Submit')
