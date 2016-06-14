6d1d17a7ab40196976af1c9df366a8f12a717366
TStand90 <tstand90@gmail.com>
2016-06-14 08:06:05 -0700

    Added 'Approved by Territory' to 'In Source' select.

--- qc_reports/element_eval_wdg.py
+++ qc_reports/element_eval_wdg.py
@@ -1665,7 +1665,7 @@ class ElementEvalLinesWdg(BaseTableElementWdg):
         scales = ['1', '2', '3', 'FYI']
         in_safe = ['No', 'Yes']
         insrc = ['No', 'Yes', 'New', 'Approved', 'Fixed', 'Not Fixed', 'Approved by Production', 'Approved by Client',
-                 'Approved as is']
+                 'Approved as is', 'Approved by Territory']
         wo_code = str(my.kwargs.get('wo_code'))
         reloaded = False
         if 'reload' in my.kwargs.keys():
